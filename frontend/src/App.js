513542e1d743c3b51da87bcef96aea9c754ca749
Priyank Vasoya <priyankvasoya7@gmail.com>
2025-08-16 16:13:10 +1000

    Landing Page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,22 @@ import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import Tasks from './pages/Tasks';
 import CreateCandidatePage from './pages/candidates/CreateCandidatePage';
 import AdminCandidatesList from './pages/candidates/AdminCandidatesList';
 import EditCandidatePage from './pages/candidates/EditCandidatePage';
 import VotePage from './pages/voting/VotePage';
 import MyVotePage from './pages/voting/MyVotePage';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/tasks" element={<Tasks />} />
         <Route path="/create-candidate" element={<CreateCandidatePage />} />
         <Route path="/list-candidates" element={<AdminCandidatesList />} />
         <Route path="/update-candidate/:id/edit" element={<EditCandidatePage />} />
--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://localhost:3.106.56.214:5001', // live
+  baseURL: 'http://3.107.192.156:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
