20202b88751e59fb5ba58f2473f5166db5a7f68f
Priyank Vasoya <priyankvasoya7@gmail.com>
2025-08-25 12:23:13 +1000

    Fix: HTTP protocol added in public IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: '3.107.31.165:5001', // live
+  baseURL: 'http://3.107.31.165:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
